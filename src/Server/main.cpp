8d9b58cfbf559b50059e8865f50a58b898aee405
npclown <nigrumspiritus@gmail.com>
2021-10-06 09:15:35 +0900

    cppcore를 사용한 server sample code

---
+++ src/Server/main.cpp
@@ -0,0 +1,14 @@
+﻿#include "stdafx.h"
+
+int main()
+{
+    ST_NETWORK_INFO stNetworkInfo;
+    stNetworkInfo.strIP = TEXT("1.2.3.4");
+    stNetworkInfo.strMac = TEXT("00:01:02:03:04:05");
+    stNetworkInfo.wPort = 1234;
+
+    std::tstring strContext;
+    core::WriteJsonToString(&stNetworkInfo, strContext);
+    tprintf("%s", strContext.c_str());
+    return 0;
+}
---
+++ src/Server/stdafx.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cstdio>
+#include <cppcore.h>
+
+struct ST_NETWORK_INFO : public core::IFormatterObject
+{
+    std::tstring strIP;
+    std::tstring strMac;
+    WORD wPort;
+
+    void OnSync(core::IFormatter& formatter)
+    {
+        formatter
+            + core::sPair(TEXT("IP"), strIP)
+            + core::sPair(TEXT("Mac"), strMac)
+            + core::sPair(TEXT("Port"), wPort)
+            ;
+    }
+};
