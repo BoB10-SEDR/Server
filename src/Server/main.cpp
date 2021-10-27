14a5656070d90a38301f70a824175eaca453f950
npclown <nigrumspiritus@gmail.com>
2021-10-27 15:15:11 +0900

    Merge branch 'develop' into Added/socket_communication
    # Conflicts:
    # src/Server/main.cpp
    # src/Server/Server.vcxproj

--- src/Server/main.cpp
+++ src/Server/main.cpp
@@ -2,6 +2,8 @@
 #include "CMessage.h"
 #include "CEpollServer.h"
 #include "Function.h"
+#include <algorithm>
+#include "RestApiServer.h"
 
 void SendTest() {
 	while (true)
@@ -67,6 +69,28 @@ int main(int argc, char* argv[])
 
 		std::future<void> a = std::async(std::launch::async, &CMessage::Init, MessageManager());
 		std::future<void> b = std::async(std::launch::async, &SendTest);
+
+		Pistache::Port port(9080);
+
+		int thr = 2;
+
+		if (argc >= 2)
+		{
+			port = static_cast<uint16_t>(std::stol(argv[1]));
+
+			if (argc == 3)
+				thr = std::stoi(argv[2]);
+		}
+
+		Pistache::Address addr(Pistache::Ipv4::any(), port);
+
+		std::cout << "Cores = " << Pistache::hardware_concurrency() << std::endl;
+		std::cout << "Using " << thr << " threads" << std::endl;
+
+		CRestApiServer stats(addr);
+
+		stats.Init(thr);
+		stats.Start();
 	}
 	catch (std::exception& e)
 	{
@@ -76,4 +100,3 @@ int main(int argc, char* argv[])
 
 	return 0;
 }
-
