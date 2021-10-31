367c4d0349e948a804b50c4bfbbc7f42e6f9f28b
CHO-YUJIN <68245063+CHO-YUJIN@users.noreply.github.com>
2021-11-01 02:13:54 +0900

    DB-connection (#19)
    * DB-connection
    * 프로젝트 구조 변경

---
+++ src/Server/dbconnection.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include <iostream>
+#include <mysql/mysql.h>
+#include <vector>
+#include <map>
+
+class ujinSQL
+{
+private:
+    std::string m_address;
+    std::string m_user;
+    std::string m_pass;
+    std::string m_database;
+    MYSQL* m_conn;
+    MYSQL_RES* m_res;
+
+public:
+    ujinSQL(const std::string& address, const std::string& user, const std::string& pass, const std::string& database); 
+    ~ujinSQL();
+
+    MYSQL_RES* ExcuteQuery(const std::string& sql_query); 
+    std::vector<std::vector<std::string>> GetQueryRow(); 
+    void PrintRecords(const std::vector<std::vector<std::string>>& records); 
+};
--- src/Server/main.cpp
+++ src/Server/main.cpp
@@ -1,9 +1,10 @@
-﻿#include "stdafx.h"
+#include "stdafx.h"
 #include "CMessage.h"
 #include "CEpollServer.h"
 #include "Function.h"
 #include <algorithm>
 #include "RestApiServer.h"
+#include "dbconnection.h"
 
 ST_ENV env;
 
@@ -132,6 +133,21 @@ int main(int argc, char* argv[])
 		core::Log_Error(TEXT("main.cpp - [%s]"), TEXT(e.what()));
 	}
 	core::Log_Info(TEXT("main.cpp - [%s]"), TEXT("Terminate Agent Program!"));
+/*
+  auto conn = new ujinSQL("12.52.135.143", "root", "autoset", "mysql"); // 생성자에서 mysql db 연결
 
+    MYSQL_RES* res = conn->ExcuteQuery("show tables"); // 쿼리 실행하기
+    std::cout << "MySQL Tables in mysql database:" << std::endl;
+    std::vector<std::vector<std::string>> records = conn->GetQueryRow();
+    conn->PrintRecords(records);
+
+    for (int i = 1; i < 3; i++) // 0은 칼럼명이라 1부터 시작
+    {
+        std::string query = "select * from ";
+        query.append(records[i][0]); // show tables의 결과에서 0번째 인덱스가 table명이므로
+        MYSQL_RES* res = conn->ExcuteQuery(query);
+        std::vector<std::vector<std::string>> records2 = conn->GetQueryRow();
+        conn->PrintRecords(records2);
+    }*/
 	return 0;
 }
