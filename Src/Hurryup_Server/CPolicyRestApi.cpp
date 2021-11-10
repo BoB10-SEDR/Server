354964199cae84f1240704daba3e280986c4c856
npclown <nigrumspiritus@gmail.com>
2021-11-11 02:37:21 +0900

    대응 정책 적용 및 해제 API

--- Src/Hurryup_Server/CPolicyRestApi.cpp
+++ Src/Hurryup_Server/CPolicyRestApi.cpp
@@ -9,8 +9,8 @@ void CPolicyRestApi::Routing(Pistache::Rest::Router& router)
     Pistache::Rest::Routes::Post(router, "/v1/policies", Pistache::Rest::Routes::bind(&CPolicyRestApi::PostPolicyInfo, this));
     Pistache::Rest::Routes::Put(router, "/v1/policies/:idx", Pistache::Rest::Routes::bind(&CPolicyRestApi::PutPolicyInfo, this));
     Pistache::Rest::Routes::Delete(router, "/v1/policies/:idx", Pistache::Rest::Routes::bind(&CPolicyRestApi::DeletePolicyInfo, this));
-    Pistache::Rest::Routes::Post(router, "/v1/policies/:idx/activate", Pistache::Rest::Routes::bind(&CPolicyRestApi::PostPolicyActivate, this));
-    Pistache::Rest::Routes::Post(router, "/v1/policies/:idx/inactivate", Pistache::Rest::Routes::bind(&CPolicyRestApi::PostPolicyInactivate, this));
+    Pistache::Rest::Routes::Post(router, "/v1/policies/:idx/activate/:device_idx", Pistache::Rest::Routes::bind(&CPolicyRestApi::PostPolicyActivate, this));
+    Pistache::Rest::Routes::Post(router, "/v1/policies/:idx/inactivate/:device_idx", Pistache::Rest::Routes::bind(&CPolicyRestApi::PostPolicyInactivate, this));
     Pistache::Rest::Routes::Get(router, "/v1/policies/:idx/download", Pistache::Rest::Routes::bind(&CPolicyRestApi::GetPolicyDownload, this));
     Pistache::Rest::Routes::Get(router, "/v1/policies/:idx/devices", Pistache::Rest::Routes::bind(&CPolicyRestApi::GetPolicyAvailableDeviceLists, this));
 }
@@ -403,15 +403,221 @@ void CPolicyRestApi::DeletePolicyInfo(const Pistache::Rest::Request& request, Pi
 
 void CPolicyRestApi::PostPolicyActivate(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response)
 {
+    core::Log_Debug(TEXT("CPolicyRestApi.cpp - [%s]"), TEXT("PostPolicyActivate"));
 
+    try {
+        nlohmann::json jsonMessage = { {"message", ""}, {"errors", nlohmann::json::array()}, {"outputs", nlohmann::json::array()} };
+
+        bool error = false;
+        int idx = -1;
+        int device_idx = -1;
+
+        if (request.hasParam(":idx")) {
+            std::string message = request.param(":idx").as<std::string>();
+            if (std::regex_match(message, regexNumber))
+                idx = atoi(message.c_str());
+            else {
+                jsonMessage["message"] = "Error";
+                jsonMessage["errors"].push_back({ {"Parameter Errors", "idx must be number."} });
+                error = true;
+            }
+        }
+
+        if (request.hasParam(":device_idx")) {
+            std::string message = request.param(":device_idx").as<std::string>();
+            if (std::regex_match(message, regexNumber))
+                device_idx = atoi(message.c_str());
+            else {
+                jsonMessage["message"] = "Error";
+                jsonMessage["errors"].push_back({ {"Parameter Errors", "device idx must be number."} });
+                error = true;
+            }
+        }
+
+        if (error)
+        {
+            response.send(Pistache::Http::Code::Bad_Request, jsonMessage.dump(), Pistache::Http::Mime::MediaType::fromString("application/json"));
+            return;
+        }
+
+        MYSQL_RES* res = dbcon.SelectQuery(TEXT("SELECT socket FROM device WHERE idx = %d;"), device_idx);
+        int agentSocket = 0;
+
+        if (res == NULL) {
+            jsonMessage["message"] = "Error";
+            jsonMessage["errors"].push_back({ {"Internal Server Errors", "Database Errors"} });
+            response.send(Pistache::Http::Code::Internal_Server_Error, jsonMessage.dump(), Pistache::Http::Mime::MediaType::fromString("application/json"));
+            return;
+        }
+
+        std::vector<MYSQL_ROW> row = CDatabase::GetRowList(res);
+
+        if (row.size() == 0) {
+            jsonMessage["message"] = "Error";
+            jsonMessage["errors"].push_back({ {"Not_Found", "Device Is Not Exisit"} });
+            response.send(Pistache::Http::Code::Not_Found, jsonMessage.dump(), Pistache::Http::Mime::MediaType::fromString("application/json"));
+            return;
+        }
+        else if (row.size() == 1) {
+            agentSocket = std::stoi(row[0][0]);
+        }
+
+        if (agentSocket == 0) {
+            jsonMessage["message"] = "Error";
+            jsonMessage["errors"].push_back({ {"Device_Error", "Device Not Connected"} });
+            response.send(Pistache::Http::Code::Not_Found, jsonMessage.dump(), Pistache::Http::Mime::MediaType::fromString("application/json"));
+            return;
+        }
+
+        res = dbcon.SelectQuery(TEXT("SELECT idx, name, isfile, apply_content FROM policy where idx= %d;"), idx);
+
+        if (res == NULL) {
+            jsonMessage["message"] = "Error";
+            jsonMessage["errors"].push_back({ {"Internal Server Errors", "Database Errors"} });
+            response.send(Pistache::Http::Code::Internal_Server_Error, jsonMessage.dump(), Pistache::Http::Mime::MediaType::fromString("application/json"));
+            return;
+        }
+
+        bool result = true;
+
+        row = CDatabase::GetRowList(res);
+        if (row.size() == 0) {
+            jsonMessage["message"] = "Error";
+            jsonMessage["errors"].push_back({ {"Not_Found", "Policy Is Not Exisit"} });
+            response.send(Pistache::Http::Code::Not_Found, jsonMessage.dump(), Pistache::Http::Mime::MediaType::fromString("application/json"));
+            return;
+        }
+
+        ST_POLICY_INFO policyInfo(atoi(row[0][0]), row[0][1], row[0][3] == NULL ? "" : row[0][3]);
+        std::tstring jsPacketSend;
+        core::WriteJsonToString(&policyInfo, jsPacketSend);
+
+        MessageManager()->PushSendMessage(agentSocket, REQUEST, "/monitoring/activate", jsPacketSend);
+
+        std::cout << jsPacketSend << std::endl;
+
+        jsonMessage["message"] = "Success";
+        response.send(Pistache::Http::Code::Ok, jsonMessage.dump(), Pistache::Http::Mime::MediaType::fromString("application/json"));
+    }
+    catch (nlohmann::json::type_error& ex)
+    {
+        nlohmann::json jsonMessage = { {"message", ""}, {"errors", nlohmann::json::array()}, {"outputs", nlohmann::json::array()} };
+        jsonMessage["message"] = "Error";
+        jsonMessage["errors"].push_back({ {"Internal Server Errors", ex.what()} });
+        response.send(Pistache::Http::Code::Ok, ex.what());
+    }
 }
 
 void CPolicyRestApi::PostPolicyInactivate(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response)
 {
+    core::Log_Debug(TEXT("CPolicyRestApi.cpp - [%s]"), TEXT("PostPolicyInactivate"));
+
+    try {
+        nlohmann::json jsonMessage = { {"message", ""}, {"errors", nlohmann::json::array()}, {"outputs", nlohmann::json::array()} };
+
+        bool error = false;
+        int idx = -1;
+        int device_idx = -1;
+
+        if (request.hasParam(":idx")) {
+            std::string message = request.param(":idx").as<std::string>();
+            if (std::regex_match(message, regexNumber))
+                idx = atoi(message.c_str());
+            else {
+                jsonMessage["message"] = "Error";
+                jsonMessage["errors"].push_back({ {"Parameter Errors", "idx must be number."} });
+                error = true;
+            }
+        }
+
+        if (request.hasParam(":device_idx")) {
+            std::string message = request.param(":device_idx").as<std::string>();
+            if (std::regex_match(message, regexNumber))
+                device_idx = atoi(message.c_str());
+            else {
+                jsonMessage["message"] = "Error";
+                jsonMessage["errors"].push_back({ {"Parameter Errors", "device idx must be number."} });
+                error = true;
+            }
+        }
+
+        if (error)
+        {
+            response.send(Pistache::Http::Code::Bad_Request, jsonMessage.dump(), Pistache::Http::Mime::MediaType::fromString("application/json"));
+            return;
+        }
+
+        MYSQL_RES* res = dbcon.SelectQuery(TEXT("SELECT socket FROM device WHERE idx = %d;"), device_idx);
+        int agentSocket = 0;
+
+        if (res == NULL) {
+            jsonMessage["message"] = "Error";
+            jsonMessage["errors"].push_back({ {"Internal Server Errors", "Database Errors"} });
+            response.send(Pistache::Http::Code::Internal_Server_Error, jsonMessage.dump(), Pistache::Http::Mime::MediaType::fromString("application/json"));
+            return;
+        }
+
+        std::vector<MYSQL_ROW> row = CDatabase::GetRowList(res);
+
+        if (row.size() == 0) {
+            jsonMessage["message"] = "Error";
+            jsonMessage["errors"].push_back({ {"Not_Found", "Device Is Not Exisit"} });
+            response.send(Pistache::Http::Code::Not_Found, jsonMessage.dump(), Pistache::Http::Mime::MediaType::fromString("application/json"));
+            return;
+        }
+        else if (row.size() == 1) {
+            agentSocket = std::stoi(row[0][0]);
+        }
+
+        if (agentSocket == 0) {
+            jsonMessage["message"] = "Error";
+            jsonMessage["errors"].push_back({ {"Device_Error", "Device Not Connected"} });
+            response.send(Pistache::Http::Code::Not_Found, jsonMessage.dump(), Pistache::Http::Mime::MediaType::fromString("application/json"));
+            return;
+        }
+
+        res = dbcon.SelectQuery(TEXT("SELECT idx, name, isfile, release_content FROM policy where idx= %d;"), idx);
+
+        if (res == NULL) {
+            jsonMessage["message"] = "Error";
+            jsonMessage["errors"].push_back({ {"Internal Server Errors", "Database Errors"} });
+            response.send(Pistache::Http::Code::Internal_Server_Error, jsonMessage.dump(), Pistache::Http::Mime::MediaType::fromString("application/json"));
+            return;
+        }
+
+        bool result = true;
+
+        row = CDatabase::GetRowList(res);
+        if (row.size() == 0) {
+            jsonMessage["message"] = "Error";
+            jsonMessage["errors"].push_back({ {"Not_Found", "Policy Is Not Exisit"} });
+            response.send(Pistache::Http::Code::Not_Found, jsonMessage.dump(), Pistache::Http::Mime::MediaType::fromString("application/json"));
+            return;
+        }
+
+        ST_POLICY_INFO policyInfo(atoi(row[0][0]), row[0][1], row[0][3] == NULL ? "" : row[0][3]);
+        std::tstring jsPacketSend;
+        core::WriteJsonToString(&policyInfo, jsPacketSend);
+
+        MessageManager()->PushSendMessage(agentSocket, REQUEST, "/monitoring/inactivate", jsPacketSend);
+
+        std::cout << jsPacketSend << std::endl;
+
+        jsonMessage["message"] = "Success";
+        response.send(Pistache::Http::Code::Ok, jsonMessage.dump(), Pistache::Http::Mime::MediaType::fromString("application/json"));
+    }
+    catch (nlohmann::json::type_error& ex)
+    {
+        nlohmann::json jsonMessage = { {"message", ""}, {"errors", nlohmann::json::array()}, {"outputs", nlohmann::json::array()} };
+        jsonMessage["message"] = "Error";
+        jsonMessage["errors"].push_back({ {"Internal Server Errors", ex.what()} });
+        response.send(Pistache::Http::Code::Ok, ex.what());
+    }
 }
 
 void CPolicyRestApi::GetPolicyDownload(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response)
 {
+
 }
 
 void CPolicyRestApi::GetPolicyAvailableDeviceLists(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response)
